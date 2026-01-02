0db8d98f523504fe1ef66928951d0471ea28d589
Max Scott <maxttocs@gmail.com>
2026-01-02 10:55:57 -0500

    Performance: Optimized font loading to reduce layout shift

--- clara_app/ui/styles.py
+++ clara_app/ui/styles.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 APP_STYLES = """
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Lato:ital,wght@0,300;0,400;0,700;1,400&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Lato:ital,wght@0,300;0,400;0,700;1,400&family=Open+Sans:wght@300;400;500;600;700&display=swap');
 
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
